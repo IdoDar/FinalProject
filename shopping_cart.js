f93925dd28e6e08ca48b3c574a64ed9a84e9c2bc
yardendvir24 <yardendvir12@gmail.com>
2024-10-19 14:36:14 +0300

    added payment page
    added payment page that checks the information you filled is valid by patterns.

--- shopping_cart.js
+++ shopping_cart.js
@@ -1,9 +1,14 @@
 let cart_icon = document.querySelector('.cart-icon');
 let close_cart = document.querySelector('.close');
+let payment_cart = document.querySelector('.pay')
 let show_cart = document.querySelector('body');
 
 cart_icon.addEventListener('click', show);
 close_cart.addEventListener('click', show);
+payment_cart.addEventListener('click', function () { window.location.href = "./payment.html"});
+
+
+
 
 
 
