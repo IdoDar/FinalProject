73d4191c8d5134a034282076d1fa3ba35a2439e2
yardendvir24 <yardendvir12@gmail.com>
2024-10-19 19:47:48 +0300

    pages for sign-in and sign up
    2 buttons of sign in and sign up where each one opens a form for you to fill - doesn't connect to DB yet.

---
+++ sign_in_up.js
@@ -0,0 +1,84 @@
+const name_pattern = /^[\u0590-\u05FFa-z\sA-Z ,.'-]+$/;
+const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phone_pattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+
+function show_sign_up(){
+    document.getElementById('sign_up_id').style.display = 'block';
+}
+
+function show_sign_in() {
+    document.getElementById('sign_in_id').style.display = 'block';
+}
+
+function func_close_in() {
+    document.getElementById('sign_in_id').style.display = 'none'
+}
+function func_close_up() {
+    document.getElementById('sign_up_id').style.display = 'none';
+}
+function check_input_up() {
+    console.log("clicked");
+    let my_message = '';
+    let problem = false;
+    let f_name = document.getElementById("fname").value;
+    let l_name = document.getElementById("lname").value;
+    let a_email = document.getElementById("email").value;
+    let n_phone = document.getElementById("phone").value;
+    let pass1 = document.getElementById("password1").value;
+    let pass2 = document.getElementById("password2").value;
+    if (!name_pattern.test(f_name)) {
+        problem = true;
+        my_message = my_message + "השם הפרטי לא תקין\n";
+    }
+    if (!name_pattern.test(l_name)) {
+        problem = true;
+        my_message = my_message + "השם משפחה לא תקין\n";
+    }
+    if (!email_pattern.test(a_email)) {
+        problem = true;
+        my_message = my_message + "כתובת המייל לא תקינה\n";
+    }
+    if (!phone_pattern.test(n_phone)) {
+        problem = true;
+        my_message = my_message + "מספר הטלפון לא תקין\n";
+    }
+    if (pass1 != pass2) {
+        problem = true;
+        my_message = my_message + "הסיסמאות לא זהות\n";
+    }
+    if (problem) {
+        alert(my_message);
+    }
+    if (!problem) {
+        let m_gender = document.getElementById("gender").value;
+        let b_date = document.getElementById("bdate").value;
+        //send all the information to DB of customers
+    }
+}
+
+function check_input_in() {
+    console.log("clicked");
+    let my_email = document.getElementById("email_id").value;
+    if (!email_pattern.test(my_email)) {
+        
+        alert("כתובת המייל לא תקינה");
+    }
+    else {
+        let pass = document.getElementById("password_id").value;
+        //check password and email to DB of customers
+    }
+}
+
+// Get the form
+var sign_up = document.getElementById('sign_up_id');
+var sign_in = document.getElementById('sign_in_id');
+
+// When the user clicks anywhere outside of the form - close it
+window.onclick = function (event) {
+    if (event.target == sign_up) {
+        sign_up.style.display = "none";
+    }
+    if (event.target == sign_in) {
+        sign_in.style.display = "none";
+    }
+}
