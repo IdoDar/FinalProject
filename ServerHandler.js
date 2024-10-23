4b29f3cf3f4f7365815e39c949241b0c7fb18cb4
Ido Dar <idodar6@gmail.com>
2024-10-23 15:59:34 +0300

    delete node_modules.zip & normalize with routes

--- ServerHandler.js
+++ ServerHandler.js
@@ -1,21 +1,22 @@
 //Express Module
-const express = require('express')
-
+const express = require('express');
+const logger = require('./middleware/logger');
+const PORT = process.env.PORT || 8080;
 //Init app http
-const app=express()
+const app=express();
 
 //make main use the "public" folder as it's gets and posts
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.set("view engine", "ejs")
 
-const authRouter = require("./routes/auth")
-app.use("/auth", authRouter)
+//routes
+app.use("/auth", require("./routes/auth"));
 
-app.listen(8080)
+app.listen(PORT, ()=> console.log('Server Running'));
 
 
 
