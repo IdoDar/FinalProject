ef734845eee4b7a69682eebb28eb4090784bf255
Ido Dar <idodar6@gmail.com>
2024-10-31 16:22:44 +0200

    change

--- routes/main.js
+++ routes/main.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 //the test is my secret route for testing JWT for supplier
 router.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, '../Views', 'test2.html'));
+  res.sendFile(path.join(__dirname, '../Views', 'home.html'));
 });
 
 router.get("/product/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, '../Views', 'product.html'));
+  res.sendFile(path.join(__dirname, '../Views', 'productPage.html'));
 });
 
 
