d15bf59990f852a5e82785305eca046c240aeec5
forcolmanwork <forcolmanwork@gmail.com>
2024-11-06 23:07:27 +0200

    Add files via upload

--- routes/API/suppliers.js
+++ routes/API/suppliers.js
@@ -65,6 +65,29 @@ router.get("/All", async (req, res) => {
         res.send(data)
 })
 
+router.post("/query", async (req, res) => {
+    var search={}
+    for(var attributename in req.body){
+        if(attributename=="phoneNum")
+            search.$expr={
+            $regexMatch: {
+              input: { $toString: "$phoneNum" },  // Cast number to string
+              regex: new RegExp(req.body["phoneNum"]),  // Regex to match the price starting with '12'
+              options: 'i'   // Case-insensitive search (if necessary)
+            }
+          }
+        else
+            search[`${attributename}`]=new RegExp(req.body[attributename],'i')
+    }
+    const out = await mongoose_api.ReadData("suppliers", search, { _id: 0 })
+    var err = out[0]
+    var data = out[1]
+    if (err)
+        res.status(500).json(err)
+    else
+        res.send(data)
+})
+
 router.get("/:suppliernum", async (req, res) => {
     const suppliernum = req.params.suppliernum.replace(/"/g, '')
     const out = await mongoose_api.ReadData("suppliers", { numCompany: suppliernum }, { _id: 1 })
